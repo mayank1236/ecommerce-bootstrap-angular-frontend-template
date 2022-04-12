d4d584b9a3a5f84997a6ee86f66c001c519ba279
Mayank <mayankgupta14042000@gmail.com>
2022-04-13 00:27:35 +0530

    Completed the template frontend

--- src/app/collections/collections.component.ts
+++ src/app/collections/collections.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-collections',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections.component.css']
 })
 export class CollectionsComponent implements OnInit {
+  faArrow = faCircleArrowRight;
+
   blocks: {heading: string, imageUrl: string}[] = [
                       {heading: 'Laptop Collection',imageUrl: './assets/img/product01.png'},
                       {heading: 'Accessories Collection',imageUrl: './assets/img/product02.png'},
