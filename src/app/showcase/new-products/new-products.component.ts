2f24f886e9de0f1a5feb6829838952b5887ffd81
Mayank <mayankgupta14042000@gmail.com>
2022-04-12 16:47:08 +0530

    add to cart button on hover (new products)

--- src/app/showcase/new-products/new-products.component.ts
+++ src/app/showcase/new-products/new-products.component.ts
@@ -12,9 +12,18 @@ export class NewProductsComponent implements OnInit {
     './assets/img/product05.png',
     './assets/img/product06.png',
   ] ;
+
+  leaving: number = NaN;
+
+  hovering: number = NaN;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onHover(index: number) {
+    this.hovering = index;
+
   }
 
 }
